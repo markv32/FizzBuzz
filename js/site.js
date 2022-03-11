351a1360b1f353c3b4798402ccc91e01cb64612a
markv32 <markanthonyv@gmail.com>
2022-03-10 22:10:51 -0600

    Updated app.html
    Updated app

--- js/site.js
+++ js/site.js
@@ -1,4 +1,48 @@
-// Call Hello World
-function helloWorld(){
-    alert("Hello World!");
-}
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+  
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+  
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+      let numbers = generateNumbers(startValue, endValue);
+    
+      //we call displayNumbers
+      displayNumbers(numbers);
+  
+    } else {
+      alert("You must enter valid number!");
+    }
+  }
+  
+  function generateNumbers(sValue, eValue) {
+    let numbers = [];
+  
+    for (let index = sValue; index <= eValue; index++) {
+      numbers.push(index);
+    }
+  
+    return numbers;
+  }
+  
+  function displayNumbers(numbers) {
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+      
+      let className = "even";
+  
+      let number = numbers[index];
+  
+      if (number % 3 === "0") {
+        className = "even";
+      } else {
+        className = "odd";
+      }
+      templateRows += `<tr><td class="${className}">${number}</td><tr>`;
+    }
+  
+    document.getElementById("results").innerHTML = templateRows;
+  }
+  
